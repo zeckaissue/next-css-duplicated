9f1035a326058a130cfd2291fea30eb29fd6b178
Robin Ferrari <roferrari@sqli.com>
2023-12-24 10:02:16 +0100

    Init

---
+++ src/app/components/client-example.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+import { Demo } from "./demo"
+
+export const ClientExample = () => {
+    return <div>
+        Demo inside client component:
+        <Demo />
+    </div>
+}
---
+++ src/app/components/demo.tsx
@@ -0,0 +1,4 @@
+import democss from './demo.module.css'
+export const Demo = () => {
+    return <div className={democss.demo}>Demo</div>
+}
